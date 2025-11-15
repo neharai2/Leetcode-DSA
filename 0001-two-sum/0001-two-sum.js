6fbd7dc63c82621615780e7039f5971d130098bd
Neha Rai <68416678+neharai2@users.noreply.github.com>
2025-11-15 22:17:37 +0100

    Time: 1 ms (87.66%), Space: 56 MB (59.93%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,11 +4,17 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
- for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] === target) {
-                return [i, j];
-            }
+
+    const map = {};
+
+    for (let i = 0; i < nums.length; i++) {
+        const comp = target - nums[i];
+
+        if(map.hasOwnProperty(comp)){
+            return [map[comp], i];
         }
+
+        map[nums[i]] = i;
     }
+    
 };
