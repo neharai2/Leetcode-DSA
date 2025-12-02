48ca09a2cf15ed0dd659adda26321e8e7b654add
Neha Rai <68416678+neharai2@users.noreply.github.com>
2025-12-02 19:30:15 +0100

    Time: 14 ms (72.72%), Space: 93.4 MB (28.24%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> numSet = new HashSet<>();
+
+        for(int n : nums){
+            if(numSet.contains(n)){
+                return true;
+            }
+            numSet.add(n);
+        }
+        return false;
+    }
+}
