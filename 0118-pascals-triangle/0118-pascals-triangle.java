950336b318923cbcf8e90b55b9aa6458567b7461
Neha Rai <68416678+neharai2@users.noreply.github.com>
2025-12-17 00:27:22 +0100

    Time: 1 ms (92.72%), Space: 43.2 MB (62.44%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,25 +1,24 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
+         List<List<Integer>> result = new ArrayList<>();
 
-     List<List<Integer>> res = new ArrayList<>();
+        // Array to store the previous iteration value
+        ArrayList<Integer> arr = new ArrayList<>();
 
-     res.add(List.of(1));
-
-     for(int i =0; i< numRows -1; i++){
-        List<Integer> dummyRow = new ArrayList<>();
-        dummyRow.add(0);
-        dummyRow.addAll(res.get(res.size()-1));
-        dummyRow.add(0);
-        List<Integer> row = new ArrayList<>();
-
-         for(int j =0; j< dummyRow.size() -1; j++){
-            row.add(dummyRow.get(j) + dummyRow.get(j+1));
-         }
-         res.add(row);
-     }
-     return res;
-
-
-        
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> ans = new ArrayList<Integer>();
+            for (int j = 0; j <= i; j++) {
+                if(j==0 || j==i){
+                    ans.add(1);
+                }else{
+                    // Sum of two numbers directly above
+                    ans.add(arr.get(j-1) + arr.get(j));
+                }
+            }
+            // Update prev array to current array for next iteration
+            arr = (ArrayList<Integer>) ans;
+            result.add(ans);
+        }
+        return result;
     }
 }
