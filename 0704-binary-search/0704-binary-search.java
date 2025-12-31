21b139a6156a432d04c0b5b8c5071fa7fda4932b
Neha Rai <68416678+neharai2@users.noreply.github.com>
2025-12-31 19:43:34 +0100

    Time: 0 ms (100%), Space: 48.7 MB (12.17%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -3,14 +3,14 @@ class Solution {
         
         int start = 0;
         int end = nums.length -1 ;
-        int mid = -1;
+        int mid;
 
         while(start <= end){
-            mid = start + (end- start)/2;
+            mid = start + ((end- start)/2);
             if(target == nums[mid]){
                 return mid;
             }
-            else if(target < mid){
+            else if(target < nums[mid]){
                 end = mid -1;
             }
             else{
