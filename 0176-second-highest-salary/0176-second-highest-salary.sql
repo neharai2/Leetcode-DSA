59018a48bd65797627309d058bf66a476a88e602
Neha Rai <68416678+neharai2@users.noreply.github.com>
2026-07-15 12:17:31 +0200

    Time: 243 ms (99.68%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,7 +1,6 @@
-# Write your MySQL query statement belo
 SELECT (
     SELECT DISTINCT salary
     FROM Employee
     ORDER BY salary DESC
     LIMIT 1 OFFSET 1
-) AS SecondHighestSalary;
+) as secondHighestSalary
